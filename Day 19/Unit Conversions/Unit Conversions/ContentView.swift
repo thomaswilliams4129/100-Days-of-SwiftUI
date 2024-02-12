ef0f7bd8a416c8fb50ac8e93f4ec5ac71fe7a843
Thomas Williams <thwilliams4129@gmail.com>
2024-02-12 18:28:31 -0500

    Day 19
    Challenge Day Started app for unit conversions

---
+++ Day 19/Unit Conversions/Unit Conversions/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Unit Conversions
+//
+//  Created by Thomas Williams on 2/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Section("Temperature conversion") {
+                
+            }
+            
+            Section("Length conversion") {
+                
+            }
+            
+            Section("Volume conversion") {
+                
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day 19/Unit Conversions/Unit Conversions/Unit_ConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConversionsApp.swift
+//  Unit Conversions
+//
+//  Created by Thomas Williams on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
