30fa5b86d324594c6a6727db34709fb779bfe830
Thomas Williams <thwilliams4129@gmail.com>
2024-02-13 21:28:29 -0500

    Day 20
    Guess the Flag: Introduction
    Using stacks to arrange views
    Colors and frames
    Gradients
    Buttons and images
    Showing alert messages

---
+++ Day 20/Guess the Flag/Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Thomas Williams on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
