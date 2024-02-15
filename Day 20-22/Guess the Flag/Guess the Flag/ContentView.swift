60b2bfffc72004a90be59277799e6f255d97c8b4
Thomas Williams <thwilliams4129@gmail.com>
2024-02-15 15:33:08 -0500

    Day 22
    ✅ Project 2: Flag Guessing Game Part 3
    * Add property to store user score
    * Alert to tell the user wrong answer
    * Make the game go to 8 questions and then show a final result

--- Day 20-22/Guess the Flag/Guess the Flag/ContentView.swift
+++ Day 20-22/Guess the Flag/Guess the Flag/ContentView.swift
@@ -14,13 +14,24 @@ struct ContentView: View {
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var userScore = 0
+    @State private var gameOver = false
+    @State private var numberOfAttempts = 0
     
     var body: some View {
         
         ZStack {
-            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
                 .ignoresSafeArea()
-            VStack(spacing: 30) {
+            
+            VStack(spacing: 15) {
+                Text("Guess the Flag")
+                       .font(.largeTitle.weight(.bold))
+                       .foregroundStyle(.white)
+                
                 VStack {
                     Text("Tap the flag of")
                         .foregroundColor(.white)
@@ -42,28 +53,61 @@ struct ContentView: View {
                     }
                     
                 }
+                Text("Current Score: \(userScore)")
+                    .font(.body.weight(.bold))
+                    .foregroundStyle(.white)
             }
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 20)
+            .background(.regularMaterial)
+            .clipShape(.rect(cornerRadius: 20))
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(userScore)")
+                .foregroundStyle(.secondary)
+                    .font(.title.bold())
         }
+        .alert("Game is Over", isPresented: $gameOver) {
+            Button("Start Over", action: resetGame)
+        } message: {
+            Text("Final Score \(userScore) / \(numberOfAttempts)")
+                .foregroundStyle(.secondary)
+                    .font(.title.bold())
+        }
+        
+        
+        
     }
     
     func flagTapped(_ number: Int) {
+        numberOfAttempts += 1
         if number == correctAnswer {
             scoreTitle = "Correct"
+            userScore += 1
+            
+            if(numberOfAttempts == 8) {
+                gameOver = true
+            }
+
         } else {
             scoreTitle = "Wrong"
         }
         
         showingScore = true
+        
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func resetGame() {
+        userScore = 0
+        numberOfAttempts = 0
     }
 }
 
