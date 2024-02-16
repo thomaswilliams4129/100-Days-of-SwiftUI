d5cf71b6c85449cafbb196fe13b042408c0fe123
Thomas Williams <thwilliams4129@gmail.com>
2024-02-16 06:44:41 -0500

    Day 23
    Custom modifiers

---
+++ Day 23/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Thomas Williams on 2/15/24.
+//
+
+import SwiftUI
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+    
+    
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
+            
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct ContentView: View {
+    
+    
+    
+    
+    var body: some View {
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
+       
+    }
+}
+
+
+
+#Preview {
+    ContentView()
+}
