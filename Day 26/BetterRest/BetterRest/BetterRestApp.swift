9371bc05ebc61c1102328440787996a644960870
Thomas Williams <thwilliams4129@gmail.com>
2024-02-19 10:53:33 -0500

    Day 26
    Basic layout done and a training model with create ML

---
+++ Day 26/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Thomas Williams on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day 26/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Thomas Williams on 2/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+                Text("When do you want to wakeup?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in:1...20)
+            }
+            .navigationTitle("BetterRest🌙")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
+        }
+    }
+    
+    func calculateBedTime() {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
