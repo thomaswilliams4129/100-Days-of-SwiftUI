6cacf583857c5f66ba9c184857c4d8c956ecdaee
Thomas Williams <thwilliams4129@gmail.com>
2024-02-21 20:33:13 -0500

    Day 28
    Replace VStacks with Sections, made the stepper a Picker showing the number of cups and removed the calculate button and made a closer to find the bed time and display it with a Text view

---
+++ Day 26-28/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,84 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Thomas Williams on 2/19/24.
+//
+
+import CoreML
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }
+    
+    var bedTime: String {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Int64(Double(hour + minute)), estimatedSleep: sleepAmount, coffee: Int64(Double(coffeeAmount)))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+
+            return sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            return "Sorry there was a problem calculating your bedtime"
+        }
+        
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                
+                Section("When do you want to wakeup?") {
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                
+                Section("Desired amount of sleep") {
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                
+                Section("Daily coffee intake") {
+                    
+                    
+                    Picker("Number of cups", selection: $coffeeAmount) {
+                        ForEach(1..<21) {
+                            Text("^[\($0) cup](inflect: true)")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section("Your recommended bedtime is") {
+                    Text("\(bedTime)")
+                }
+            }
+            .navigationTitle("BetterRest🌙")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
