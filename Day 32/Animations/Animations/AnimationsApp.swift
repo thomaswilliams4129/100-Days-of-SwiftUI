5b65e01c0bf56d2ddaa6216b370b46f5e2f9223c
Thomas Williams <thwilliams4129@gmail.com>
2024-02-25 15:37:51 -0500

    Day 32

---
+++ Day 32/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Thomas Williams on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day 32/Animations/Animations/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Thomas Williams on 2/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var animationAmount = 1.0
+    var body: some View {
+        Button("Tap me") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(.circle)
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.default, value: animationAmount)
+    }
+}
+
+#Preview {
+    ContentView()
+}
