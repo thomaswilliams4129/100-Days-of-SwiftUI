37d828a01dfb7e2729449898c11d40e33dc8b228
Thomas Williams <thwilliams4129@gmail.com>
2024-03-03 15:25:31 -0500

    Day 39
    ScrollView, Resizing images, and using NavigationLink to push new views

---
+++ Day 39-42/Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Thomas Williams on 3/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            List(0..<100) { row in
+                NavigationLink("Row \(row)") {
+                    Text("Detail \(row)")
+                }
+            }
+            .navigationTitle("SwiftUI")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
