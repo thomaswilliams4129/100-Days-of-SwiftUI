b28b0704149c482b3a0c1fa7505345e8bed1672a
Thomas Williams <thwilliams4129@gmail.com>
2024-03-05 18:28:30 -0500

    Day 41

---
+++ Day 39-42/Moonshot/Moonshot/Color-Theme.swift
@@ -0,0 +1,18 @@
+//
+//  Color-Theme.swift
+//  Moonshot
+//
+//  Created by Thomas Williams on 3/5/24.
+//
+
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackgound: Color {
+        Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+    
+    static var lightBackground: Color {
+            Color(red: 0.2, green: 0.2, blue: 0.3)
+        }
+}
