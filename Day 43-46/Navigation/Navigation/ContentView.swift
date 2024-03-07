84fa74eca9f3c487ef05a2aace1a942234e3eae1
Thomas Williams <thwilliams4129@gmail.com>
2024-03-07 12:14:16 -0500

    Day 43

---
+++ Day 43-46/Navigation/Navigation/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by Thomas Williams on 3/7/24.
+//
+
+import SwiftUI
+
+struct Student: Hashable {
+    var id = UUID()
+    var name: String
+    var age: Int
+}
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            List(0..<100) { i in
+                NavigationLink("Select \(i)", value: i)
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected \(selection)")
+            }
+            .navigationDestination(for: Student.self) { student in
+                Text("You Selected \(student.name)")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day 43-46/Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Thomas Williams on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
