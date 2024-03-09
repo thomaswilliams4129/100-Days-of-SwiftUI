43258be9f3ff560d1b0aa10e10d76222365c233c
Thomas Williams <thwilliams4129@gmail.com>
2024-03-09 10:35:58 -0500

    Day 45
    •Customizing the navigation bar appearance
    •Placing toolbar buttons in exact locations
    •Making your navigation title editable

--- Day 43-46/Navigation/Navigation/ContentView.swift
+++ Day 43-46/Navigation/Navigation/ContentView.swift
@@ -8,31 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var path = NavigationPath()
-
+    @State private var title = "SwiftUI"
     var body: some View {
-        NavigationStack(path: $path) {
-            List {
-                ForEach(0..<5) { i in
-                    NavigationLink("Select Number: \(i)", value: i)
-                }
-                
-                ForEach(0..<5) { i in
-                    NavigationLink("Select String: \(i)", value: String(i))
-                }
-            }
-            .toolbar {
-                Button("Push 556"){
-                    path.append(556)
-                }
-            }
-            .navigationDestination(for: Int.self) { selection in
-                Text("You selected the number \(selection)")
-            }
-            .navigationDestination(for: String.self) { selection in
-                Text("You selected the string \(selection)")
-            }
-            
+        NavigationStack {
+            Text("Hello, world!")
+                .navigationTitle($title)
+                .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
