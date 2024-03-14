ae89b5bae646bfa42c2632d1c4385777bb32bcc4
Thomas Williams <thwilliams4129@gmail.com>
2024-03-13 20:34:35 -0400

    Day 49
    Networking

---
+++ Day 49/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Thomas Williams on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
