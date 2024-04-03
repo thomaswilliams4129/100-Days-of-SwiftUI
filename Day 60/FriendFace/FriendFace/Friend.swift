7c0503890686399342c64626ab9bb5e0c32e7bec
Thomas Williams <thwilliams4129@gmail.com>
2024-04-02 22:00:17 -0400

    Day 61
    Setup Models

--- Day 60/FriendFace/FriendFace/Friend.swift
+++ Day 60/FriendFace/FriendFace/Friend.swift
@@ -6,8 +6,32 @@
 //
 
 import Foundation
+import SwiftData
 
-struct Friend: Codable, Identifiable {
+@Model
+class Friend: Codable, Identifiable {
+    enum CodingKeys: CodingKey {
+        case id
+        case name
+    }
+    
     var id: UUID
     var name: String
+    
+    init(id: UUID, name: String) {
+        self.id = id
+        self.name = name
+    }
+    
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+    }
 }
--- Day 60/FriendFace/FriendFace/User.swift
+++ Day 60/FriendFace/FriendFace/User.swift
@@ -6,8 +6,24 @@
 //
 
 import Foundation
+import SwiftData
 
-struct User: Codable, Identifiable {
+@Model
+class User: Codable, Identifiable {
+    
+    enum CodingKeys: CodingKey {
+        case id
+        case isActive
+        case name
+        case age
+        case email
+        case address
+        case about
+        case registered
+        case tags
+        case friends
+    }
+    
     var id: UUID
     var isActive: Bool
     var name: String
@@ -18,4 +34,45 @@ struct User: Codable, Identifiable {
     var registered: String
     var tags: [String]
     var friends: [Friend]
+    
+    init(id: UUID, isActive: Bool, name: String, age: Int, email: String, address: String, about: String, registered: String, tags: [String], friends: [Friend]) {
+        self.id = id
+        self.isActive = isActive
+        self.name = name
+        self.age = age
+        self.email = email
+        self.address = address
+        self.about = about
+        self.registered = registered
+        self.tags = tags
+        self.friends = friends
+    }
+    
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        isActive = try container.decode(Bool.self, forKey: .isActive)
+        name = try container.decode(String.self, forKey: .name)
+        age = try container.decode(Int.self, forKey: .age)
+        email = try container.decode(String.self, forKey: .email)
+        address = try container.decode(String.self, forKey: .address)
+        about = try container.decode(String.self, forKey: .about)
+        registered = try container.decode(String.self, forKey: .registered)
+        tags = try container.decode([String].self, forKey: .tags)
+        friends = try container.decode([Friend].self, forKey: .friends)
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(isActive, forKey: .isActive)
+        try container.encode(name, forKey: .name)
+        try container.encode(age, forKey: .age)
+        try container.encode(email, forKey: .email)
+        try container.encode(address, forKey: .address)
+        try container.encode(about, forKey: .about)
+        try container.encode(registered, forKey: .registered)
+        try container.encode(tags, forKey: .tags)
+        try container.encode(friends, forKey: .friends)
+    }
 }
